0c868932eb3e1e047e8c0b80d8d68a6fef5059f6
Kunzeu <kunjohn24@gmai.com>
2023-07-09 01:47:26 -0500

    test

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -82,11 +82,10 @@ WSGI_APPLICATION = 'portafolio.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {dj_database_url.config(
-        default='postgresql://postgres:postgres@localhost/postgres',
-        conn_max_age=600
+'default': dj_database_url.config(
+    default='postgresql://postgres:postgres@localhost:5432/mysite',
+conn_max_age=600
     )
-    }
 }
 
 # Password validation
