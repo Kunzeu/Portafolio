6bc80b50e86138431b9f55609f474d06594a45bb
Kunzeu <kunjohn24@gmai.com>
2023-07-09 19:52:41 -0500

    I dont Know

--- portafolio/settings.py
+++ portafolio/settings.py
@@ -1,6 +1,7 @@
 
 from pathlib import Path
 import os
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -81,7 +82,6 @@ DATABASES = {
 }
 '''
 
-import dj_database_url
 
 DATABASES = {
     'default': dj_database_url.parse(env('DATABASE_URL'))
